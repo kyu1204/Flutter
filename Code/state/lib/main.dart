12426823e793e8c39b18b1d407318339f240522d
MINT <pb1123love@gmail.com>
2023-01-30 19:38:01 +0900

    vault backup: 2023-01-30 19:38:01

---
+++ Code/state/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: onClicked,
+                iconSize: 40,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
