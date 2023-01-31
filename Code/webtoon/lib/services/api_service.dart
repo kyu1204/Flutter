9eaa01fbc941127066dda7ad57a703422dbccb54
MINT <pb1123love@gmail.com>
2023-01-31 19:33:03 +0900

    vault backup: 2023-01-31 19:33:03

--- Code/webtoon/lib/services/api_service.dart
+++ Code/webtoon/lib/services/api_service.dart
@@ -1,4 +1,16 @@
+import 'package:http/http.dart' as http;
+
 class APIService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev/";
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String toay = "today";
+
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$toay');
+    final resposne = await http.get(url);
+    if (resposne.statusCode == 200) {
+      print(resposne.body);
+      return;
+    }
+    throw Error();
+  }
 }
