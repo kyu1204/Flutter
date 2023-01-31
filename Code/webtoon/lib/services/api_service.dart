fd1a0e6add1e409ac158c61e933970c642ac20fe
MINT <pb1123love@gmail.com>
2023-02-01 00:49:16 +0900

    vault backup: 2023-02-01 00:49:16

--- Code/webtoon/lib/services/api_service.dart
+++ Code/webtoon/lib/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_detail_model.dart';
 import 'package:webtoon/models/webtoon_model.dart';
 
 class APIService {
@@ -10,7 +11,7 @@ class APIService {
 
   static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
-    final url = Uri.parse('$baseUrl/$toay');
+    final url = Uri.parse("$baseUrl/$toay");
     final resposne = await http.get(url);
 
     if (resposne.statusCode == 200) {
@@ -19,6 +20,17 @@ class APIService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
     }
     throw Error();
   }
