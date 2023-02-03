a0eec51cb723c661ce34b4e2e00397eafa0fdadc
MINT <pb1123love@gmail.com>
2023-02-03 18:49:24 +0900

    vault backup: 2023-02-03 18:49:24

--- Codes/tictok_clone/lib/screens/sign_up/sign_up_screen.dart
+++ Codes/tictok_clone/lib/screens/sign_up/sign_up_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tictok_clone/constants/gaps.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: const [
+            Gaps.v80,
+            Text("Sign up for TikTok"),
+            Gaps.v20,
+            Text(
+                "Create a profile, follow other accounts, make your own videos, and more."),
+          ],
+        ),
+      ),
+    );
   }
 }
