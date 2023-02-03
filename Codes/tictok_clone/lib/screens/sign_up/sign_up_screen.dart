e9539569cdfd8089d900af62f46a1d2e2cee7c5e
MINT <pb1123love@gmail.com>
2023-02-03 18:59:46 +0900

    vault backup: 2023-02-03 18:59:45

--- Codes/tictok_clone/lib/screens/sign_up/sign_up_screen.dart
+++ Codes/tictok_clone/lib/screens/sign_up/sign_up_screen.dart
@@ -33,8 +33,26 @@ class SignUpScreen extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: const [Text("Already have an account?")],
+        color: Colors.grey.shade100,
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size32,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Already have an account?"),
+              Gaps.h5,
+              Text(
+                'Log in',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
