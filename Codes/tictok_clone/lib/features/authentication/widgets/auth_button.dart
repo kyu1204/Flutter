a852e5ada3ed67e07f9cdc4a4384377c486c1f6d
MINT <pb1123love@gmail.com>
2023-02-04 21:50:19 +0900

    vault backup: 2023-02-04 21:50:19

--- Codes/tictok_clone/lib/features/authentication/widgets/auth_button.dart
+++ Codes/tictok_clone/lib/features/authentication/widgets/auth_button.dart
@@ -5,38 +5,43 @@ import 'package:tictok_clone/constants/sizes.dart';
 class AuthButton extends StatelessWidget {
   final String text;
   final FaIcon icon;
+  final void Function(BuildContext) onTapFunc;
 
   const AuthButton({
     super.key,
     required this.text,
     required this.icon,
+    required this.onTapFunc,
   });
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: Container(
-        padding: const EdgeInsets.all(Sizes.size14),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.grey.shade300,
-            width: Sizes.size1,
-          ),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Align(alignment: Alignment.centerLeft, child: icon),
-            Text(
-              text,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.w600,
-              ),
+    return GestureDetector(
+      onTap: () => onTapFunc(context),
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Container(
+          padding: const EdgeInsets.all(Sizes.size14),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey.shade300,
+              width: Sizes.size1,
             ),
-          ],
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Align(alignment: Alignment.centerLeft, child: icon),
+              Text(
+                text,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
