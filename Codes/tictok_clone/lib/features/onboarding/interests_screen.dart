dc3b9121d1e7832b381df5578f087055a0c52f9e
MINT <pb1123love@gmail.com>
2023-02-10 21:54:26 +0900

    vault backup: 2023-02-10 21:54:25

--- Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
+++ Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
@@ -54,7 +54,11 @@ class InterestsScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          padding: const EdgeInsets.only(
+            left: Sizes.size24,
+            right: Sizes.size24,
+            bottom: Sizes.size16,
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -112,7 +116,7 @@ class InterestsScreen extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        elevation: 1,
+        elevation: 2,
         child: Padding(
           padding: const EdgeInsets.only(
             bottom: Sizes.size40,
