fdb18831997138b85eff4673365c6c5119835f51
MINT <pb1123love@gmail.com>
2023-02-10 16:34:29 +0900

    vault backup: 2023-02-10 16:34:29

--- Codes/tictok_clone/lib/features/authentication/login_form_screen.dart
+++ Codes/tictok_clone/lib/features/authentication/login_form_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-class LoginFormScreen extends StatelessWidget {
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+    );
   }
 }
