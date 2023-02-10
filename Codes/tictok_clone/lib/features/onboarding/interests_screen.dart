d1e615b7d0d6828ae3176fb7481163da89a62691
MINT <pb1123love@gmail.com>
2023-02-10 21:37:06 +0900

    vault backup: 2023-02-10 21:37:06

--- Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
+++ Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
@@ -2,6 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
 
+const interests = [
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+];
+
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
 
@@ -13,18 +54,47 @@ class InterestsScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-        child: Column(children: const [
-          Gaps.v32,
-          Text(
-            "Choose your interests",
-            style: TextStyle(
-              fontSize: Sizes.size40,
-              fontWeight: FontWeight.w600,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v32,
+            const Text(
+              "Choose your interests",
+              style: TextStyle(
+                fontSize: Sizes.size40,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Gaps.v20,
-          Text("Ge better video recommendations"),
-        ]),
+            Gaps.v20,
+            const Text(
+              "Ge better video recommendations",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+              ),
+            ),
+            Gaps.v64,
+            for (var interest in interests)
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size24,
+                  horizontal: Sizes.size12,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    Sizes.size32,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      blurRadius: 5,
+                      spreadRadius: 5,
+                    )
+                  ],
+                ),
+                child: Text(interest),
+              )
+          ],
+        ),
       ),
     );
   }
