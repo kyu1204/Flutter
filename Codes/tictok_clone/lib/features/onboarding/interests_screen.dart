c46ba3f757b8fe3c432cf6040e6935307cefb067
MINT <pb1123love@gmail.com>
2023-02-10 22:22:04 +0900

    vault backup: 2023-02-10 22:22:04

--- Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
+++ Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
@@ -52,13 +52,25 @@ class InterestsScreen extends StatefulWidget {
 
 class _InterestsScreenState extends State<InterestsScreen> {
   final ScrollController _scrollController = ScrollController();
+  bool _showTitle = false;
+
+  void _onScroll() {
+    if (_scrollController.offset > 100) {
+      if (_showTitle) return;
+      setState(() {
+        _showTitle = true;
+      });
+    } else {
+      setState(() {
+        _showTitle = false;
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    _scrollController.addListener(() {
-      print(_scrollController.offset);
-    });
+    _scrollController.addListener(_onScroll);
   }
 
   @override
@@ -71,7 +83,11 @@ class _InterestsScreenState extends State<InterestsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Choose your interests"),
+        title: AnimatedOpacity(
+          opacity: _showTitle ? 1 : 0,
+          duration: const Duration(milliseconds: 300),
+          child: const Text("Choose your interests"),
+        ),
       ),
       body: Scrollbar(
         controller: _scrollController,
