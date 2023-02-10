38832769ad62f01b507d8391511d7bf18d72550a
MINT <pb1123love@gmail.com>
2023-02-10 22:28:12 +0900

    vault backup: 2023-02-10 22:28:12

--- Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
+++ Codes/tictok_clone/lib/features/onboarding/interests_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
+import 'package:tictok_clone/features/onboarding/widgets/interest_button.dart';
 
 const interests = [
   "Daily Life",
@@ -123,32 +124,7 @@ class _InterestsScreenState extends State<InterestsScreen> {
                   spacing: 15,
                   children: [
                     for (var interest in interests)
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: Sizes.size16,
-                          horizontal: Sizes.size24,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(
-                            Sizes.size32,
-                          ),
-                          border: Border.all(
-                            color: Colors.black.withOpacity(0.1),
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.05),
-                              blurRadius: 5,
-                              spreadRadius: 5,
-                            )
-                          ],
-                        ),
-                        child: Text(
-                          interest,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      )
+                      InterestButton(interest: interest)
                   ],
                 )
               ],
