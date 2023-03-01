803c0fc7b2902bd539f6e451d5f63c0bf2aa28a7
MINT <pb1123love@gmail.com>
2023-03-01 18:32:45 +0900

    vault backup: 2023-03-01 18:32:45

--- Codes/tictok_clone/lib/features/main_navigation/widgets/post_video_button.dart
+++ Codes/tictok_clone/lib/features/main_navigation/widgets/post_video_button.dart
@@ -2,111 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tictok_clone/constants/sizes.dart';
 
-class PostVideoButton extends StatefulWidget {
-  const PostVideoButton({
-    super.key,
-    required this.isHover,
-  });
-
-  final bool isHover;
-
-  @override
-  State<PostVideoButton> createState() => _PostVideoButtonState();
-}
-
-class _PostVideoButtonState extends State<PostVideoButton>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-      lowerBound: 0.0,
-      upperBound: 0.2,
-    )..addListener(() {});
-
-    setState(() {
-      if (widget.isHover) {
-        _controller.forward();
-      } else {
-        _controller.reverse();
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Transform.scale(
-      scale: widget.isHover ? 1.2 : 1,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Positioned(
-              right: 20,
-              child: Container(
-                height: 30,
-                width: 25,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size8,
-                ),
-                decoration: BoxDecoration(
-                  color: const Color(0xFF61D4F0),
-                  borderRadius: BorderRadius.circular(
-                    Sizes.size8,
-                  ),
-                ),
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          right: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xFF61D4F0),
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
               ),
             ),
-            Positioned(
-              left: 20,
-              child: Container(
-                height: 30,
-                width: 25,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size8,
-                ),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(
-                    Sizes.size8,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 30,
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size12,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(
-                  Sizes.size6,
-                ),
-              ),
-              child: const Center(
-                child: FaIcon(
-                  FontAwesomeIcons.plus,
-                  color: Colors.black,
-                  size: Sizes.size16 + Sizes.size2,
-                ),
-              ),
-            )
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          left: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: 30,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              Sizes.size6,
+            ),
+          ),
+          child: const Center(
+            child: FaIcon(
+              FontAwesomeIcons.plus,
+              color: Colors.black,
+              size: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
