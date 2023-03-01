c89a730018fc6f20278ba0158cff945f59e80af5
MINT <pb1123love@gmail.com>
2023-03-01 18:27:38 +0900

    vault backup: 2023-03-01 18:27:38

--- Codes/tictok_clone/lib/features/main_navigation/widgets/post_video_button.dart
+++ Codes/tictok_clone/lib/features/main_navigation/widgets/post_video_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/sizes.dart';
 
 class PostVideoButton extends StatefulWidget {
   const PostVideoButton({
@@ -14,8 +16,7 @@ class PostVideoButton extends StatefulWidget {
 
 class _PostVideoButtonState extends State<PostVideoButton>
     with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
-  Animation? _animation;
+  late AnimationController _controller;
 
   @override
   void initState() {
@@ -24,97 +25,86 @@ class _PostVideoButtonState extends State<PostVideoButton>
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 2),
-    )..repeat(reverse: true);
+      lowerBound: 0.0,
+      upperBound: 0.2,
+    )..addListener(() {});
 
-    _animation = Tween<double>(
-      begin: 1.0,
-      end: 1.2,
-    ).animate(_controller!);
+    setState(() {
+      if (widget.isHover) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
   }
 
   @override
   void dispose() {
-    _controller?.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // return Transform.scale(
-    //   scale: widget.isHover ? 1.2 : 1,
-    //   child: AnimatedContainer(
-    //     duration: const Duration(milliseconds: 300),
-    //     child: Stack(
-    //       clipBehavior: Clip.none,
-    //       children: [
-    //         Positioned(
-    //           right: 20,
-    //           child: Container(
-    //             height: 30,
-    //             width: 25,
-    //             padding: const EdgeInsets.symmetric(
-    //               horizontal: Sizes.size8,
-    //             ),
-    //             decoration: BoxDecoration(
-    //               color: const Color(0xFF61D4F0),
-    //               borderRadius: BorderRadius.circular(
-    //                 Sizes.size8,
-    //               ),
-    //             ),
-    //           ),
-    //         ),
-    //         Positioned(
-    //           left: 20,
-    //           child: Container(
-    //             height: 30,
-    //             width: 25,
-    //             padding: const EdgeInsets.symmetric(
-    //               horizontal: Sizes.size8,
-    //             ),
-    //             decoration: BoxDecoration(
-    //               color: Theme.of(context).primaryColor,
-    //               borderRadius: BorderRadius.circular(
-    //                 Sizes.size8,
-    //               ),
-    //             ),
-    //           ),
-    //         ),
-    //         Container(
-    //           height: 30,
-    //           padding: const EdgeInsets.symmetric(
-    //             horizontal: Sizes.size12,
-    //           ),
-    //           decoration: BoxDecoration(
-    //             color: Colors.white,
-    //             borderRadius: BorderRadius.circular(
-    //               Sizes.size6,
-    //             ),
-    //           ),
-    //           child: const Center(
-    //             child: FaIcon(
-    //               FontAwesomeIcons.plus,
-    //               color: Colors.black,
-    //               size: Sizes.size16 + Sizes.size2,
-    //             ),
-    //           ),
-    //         )
-    //       ],
-    //     ),
-    //   ),
-    // );
-    return Center(
-      child: AnimatedBuilder(
-        animation: _animation!,
-        builder: (context, child) {
-          return Transform.scale(
-            scale: _animation?.value,
-            child: child,
-          );
-        },
-        child: Container(
-          width: 100,
-          height: 100,
-          color: Colors.blue,
+    return Transform.scale(
+      scale: widget.isHover ? 1.2 : 1,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              right: 20,
+              child: Container(
+                height: 30,
+                width: 25,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size8,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF61D4F0),
+                  borderRadius: BorderRadius.circular(
+                    Sizes.size8,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              child: Container(
+                height: 30,
+                width: 25,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size8,
+                ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(
+                    Sizes.size8,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 30,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size12,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                  Sizes.size6,
+                ),
+              ),
+              child: const Center(
+                child: FaIcon(
+                  FontAwesomeIcons.plus,
+                  color: Colors.black,
+                  size: Sizes.size16 + Sizes.size2,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
