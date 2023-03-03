e9c3885e30e19aa0c75a1e53ad3df63e244dd3ad
MINT <pb1123love@gmail.com>
2023-03-03 14:51:09 +0900

    vault backup: 2023-03-03 14:51:09

---
+++ Codes/tictok_clone/lib/features/videos/video_timeline_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class VideoTimelineScreen extends StatefulWidget {
+  const VideoTimelineScreen({super.key});
+
+  @override
+  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+}
+
+class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
