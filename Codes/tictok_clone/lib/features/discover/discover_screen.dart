36cbd0dd4297e28b6dd11f64f360123965cc07df
MINT <pb1123love@gmail.com>
2023-03-10 17:22:12 +0900

    vault backup: 2023-03-10 17:22:12

--- Codes/tictok_clone/lib/features/discover/discover_screen.dart
+++ Codes/tictok_clone/lib/features/discover/discover_screen.dart
@@ -79,29 +79,42 @@ class DiscoverScreen extends StatelessWidget {
                       ),
                     ),
                     Gaps.v8,
-                    Row(
-                      children: const [
-                        CircleAvatar(
-                          radius: 15,
-                          backgroundImage: NetworkImage(
-                              "https://avatars.githubusercontent.com/u/33680799?v=4"),
-                        ),
-                        Gaps.h4,
-                        Expanded(
-                          child: Text(
-                            "My avatar is going to be very long.",
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      child: Row(
+                        children: [
+                          const CircleAvatar(
+                            radius: 12,
+                            backgroundImage: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/33680799?v=4"),
                           ),
-                        ),
-                        Gaps.h4,
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size14,
-                        ),
-                        Gaps.h2,
-                        Text("2.5M"),
-                      ],
+                          Gaps.h4,
+                          Expanded(
+                            child: Text(
+                              "My avatar is going to be very long.",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.grey.shade600,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          Gaps.h4,
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size16,
+                            color: Colors.grey.shade600,
+                          ),
+                          Gaps.h2,
+                          const Text(
+                            "2.5M",
+                          ),
+                        ],
+                      ),
                     )
                   ],
                 ),
